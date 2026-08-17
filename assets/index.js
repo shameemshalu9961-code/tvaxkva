ac7aa808678d315b3a325f84fb197b960a82bc28
shameemshalu9961-code <shameemshalu9961@gmail.com>
2026-08-17 22:09:53 +0530

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,37 +1,36 @@
-import {u as G, r as n, j as r, k as V, b as T, T as Y, s as $, d as z, A as U, m as P, X as B, K as J} from "./index-n3ykPomq.js";
-import {S as Q} from "./tva-streamer-dashboard-C1fSiCd7.js";
-import {n as R, i as W, f as q, K as X} from "./youtube-channel-image-DSWVRi7S.js";
-import {b as Z, L as ee, f as te, d as re, e as ae, h as se, t as oe} from "./live-section-header-CeyQggLU.js";
-import {P as ne} from "./profiles-faction-card-Bwq8p_gA.js";
-import {c as ie, P as le, f as ce} from "./profiles-roster-stats-DBzJgE4x.js";
-import "./profile-theme-Cb8zLgmu.js";
-import {S as ue} from "./site-footer-B3msC5PA.js";
-import "./search-COieBnn3.js";
-import "./tva-brand-logo-BgLoBUDb.js";
-import "./refresh-cw-Bsze8eOY.js";
-import "./streamer-share-Dv2OuV-N.js";
-import "./trending-up-DvwrYh2G.js";
-import "./eye-CAXkqK0e.js";
-import "./external-link-BlRFC6ja.js";
-import "./clock-DtvdPvJF.js";
-import "./arrow-right-DJuyCAft.js";
-function de(e) {
-    return {
-        ...e,
-        profileSlug: V(e.displayName)
+const __vite__mapDeps = (i, m=__vite__mapDeps, d=(m.f || (m.f = ["assets/youtube-BAdAYldv.js", "assets/tva-streamer-dashboard-MmABZltP.js", "assets/tva-streamer-dashboard-C1fSiCd7.js", "assets/site-footer-B3msC5PA.js", "assets/search-COieBnn3.js", "assets/tva-brand-logo-BgLoBUDb.js", "assets/refresh-cw-Bsze8eOY.js", "assets/tva-streamer-dashboard-D8bfesC3.css", "assets/youtube-channel-image-DSWVRi7S.js", "assets/live-section-header-CeyQggLU.js", "assets/streamer-share-Dv2OuV-N.js", "assets/trending-up-DvwrYh2G.js", "assets/eye-CAXkqK0e.js", "assets/external-link-BlRFC6ja.js", "assets/clock-DtvdPvJF.js", "assets/live-section-header-CiUc9v7K.css", "assets/live-status-grace-72eKl0kF.js", "assets/tva-DrEfQGJ9.js", "assets/gang-roster-page-DYbZJMVs.js", "assets/use-profile-roster-DqDHTLp9.js", "assets/radio-DRh3lHme.js", "assets/profiles-roster-player-card-C5KwBrhF.js", "assets/profile-theme-Cb8zLgmu.js", "assets/profile-theme-BrY5Iiw_.css", "assets/profiles-roster-stats-DBzJgE4x.js", "assets/gang-roster-page-BVnrO7nM.css", "assets/profiles-B_NX1AX1.js", "assets/profiles-faction-card-Bwq8p_gA.js", "assets/arrow-right-DJuyCAft.js", "assets/privacy-Dyq6IE5p.js", "assets/others-B-xnmzUN.js", "assets/kva-vq9qnEK0.js", "assets/kick-Dbm74dAp.js", "assets/admin-vA-PQ8iM.js", "assets/_slug-jAiEFiX7.js", "assets/twitter-BJEEp2za.js", "assets/youtube-hJ9jtipb.js", "assets/index-yvruASib.js", "assets/index-B_WqiFDv.js", "assets/megaphone-ClDb-J2K.js", "assets/loader-circle-CE0StrtM.js", "assets/index-DtqoT3qm.js", "assets/blog-category-sidebar-BPoHc1rO.js", "assets/_slug-BIbU-H1D.js", "assets/streamers-D_uPCyCr.js", "assets/admin-layout-DuXnamOm.js", "assets/upload-8OQFmPN7.js", "assets/trash-2-CL61k4Vb.js", "assets/save-CbiqeaGB.js", "assets/login-D7q15tiS.js", "assets/events-Gs8ANqCj.js", "assets/event-COxnUGQ-.js", "assets/dashboard-DWKenJre.js"]))) => i.map(i => d[i]);
+function iE(t, n) {
+    for (var s = 0; s < n.length; s++) {
+        const o = n[s];
+        if (typeof o != "string" && !Array.isArray(o)) {
+            for (const l in o)
+                if (l !== "default" && !(l in t)) {
+                    const u = Object.getOwnPropertyDescriptor(o, l);
+                    u && Object.defineProperty(t, l, u.get ? u : {
+                        enumerable: !0,
+                        get: () => o[l]
+                    })
+                }
+        }
     }
+    return Object.freeze(Object.defineProperty(t, Symbol.toStringTag, {
+        value: "Module"
+    }))
 }
-function me({streamers: e}) {
-    const t = G()
-      , s = n.useMemo( () => e.map(de), [e])
-      , o = n.useMemo( () => ie(s), [s])
-      , i = l => {
-        const d = ce(l).gangPath;
-        t({
-            to: d ?? "/profiles"
-        })
-    }
-    ;
-    return r.jsxs("div", {
-        className: "home-offline-roster mt-10 w-full border-t border-white/10 pt-10 text-left",
-
+function fw(t) {
+    return t && t.__esModule && Object.prototype.hasOwnProperty.call(t, "default") ? t.default : t
+}
+var yd = {
+    exports: {}
+}
+  , ar = {};
+var Tv;
+function oE() {
+    if (Tv)
+        return ar;
+    Tv = 1;
+    var t = Symbol.for("react.transitional.element")
+      , n = Symbol.for("react.fragment");
+    function s(o, l, u) {
+        var f = null;
+        if (u !== void 0 && (f = "" + u),
