03beeee73c78fdecc5cc9a44e4299f7c1fcb7722
shameemshalu9961-code <shameemshalu9961@gmail.com>
2026-08-23 12:26:50 +0530

    Create arrow-right-DJuyCAft.js

---
+++ assets/arrow-right-DJuyCAft.js
@@ -0,0 +1,10 @@
+import {a as o} from "./index-n3ykPomq.js";
+const r = [["path", {
+    d: "M5 12h14",
+    key: "1ays0h"
+}], ["path", {
+    d: "m12 5 7 7-7 7",
+    key: "xquz4c"
+}]]
+  , a = o("arrow-right", r);
+export {a as A};
