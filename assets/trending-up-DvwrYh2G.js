c0fc482c26f950899a84dd503685a0563142cef0
shameemshalu9961-code <shameemshalu9961@gmail.com>
2026-08-23 12:38:24 +0530

    Create trending-up-DvwrYh2G.js

---
+++ assets/trending-up-DvwrYh2G.js
@@ -0,0 +1,15 @@
+import {a as o} from "./index-n3ykPomq.js";
+const t = [["path", {
+    d: "m9 18 6-6-6-6",
+    key: "mthhwq"
+}]]
+  , c = o("chevron-right", t);
+const e = [["path", {
+    d: "M16 7h6v6",
+    key: "box55l"
+}], ["path", {
+    d: "m22 7-8.5 8.5-5-5L2 17",
+    key: "1t1m79"
+}]]
+  , h = o("trending-up", e);
+export {c as C, h as T};
